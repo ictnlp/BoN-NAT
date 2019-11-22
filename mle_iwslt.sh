34429e4ca0bff559323fe8e2fe2b3b34c597e415
shaochenze <42638348+shaochenze@users.noreply.github.com>
2019-11-22 10:11:58 +0800

    Delete mle_iwslt.sh

--- mle_iwslt.sh
+++
@@ -1,2 +0,0 @@
-python run.py --dataset iwslt-ende --vocab_size 40000 --gpu 0 --ffw_block highway --params small --lr_schedule anneal --fast --train_repeat_dec 1 --valid_repeat_dec 1 --use_argmax --use_distillation --max_len 100
-
