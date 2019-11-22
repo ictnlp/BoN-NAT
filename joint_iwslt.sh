93bb4cb2b45306c5be30244f3969457d7c403e2b
shaochenze <42638348+shaochenze@users.noreply.github.com>
2019-11-22 10:12:09 +0800

    Delete joint_iwslt.sh

--- joint_iwslt.sh
+++
@@ -1,1 +0,0 @@
-python run.py --n 2 --joint --alph 0.1 --dataset iwslt-ende --vocab_size 40000 --gpu 0 --ffw_block highway --params small --lr_schedule anneal --fast --train_repeat_dec 1 --valid_repeat_dec 1 --use_argmax  --use_distillation
